d94cb3f646967c3e7d9f65fb2aaea3518f49e47c
Mirza <devmirzaofficial@gmail.com>
2025-07-13 00:28:58 +0500

    Update index.ts

--- src/providers/sources/ridomovies/index.ts
+++ src/providers/sources/ridomovies/index.ts
@@ -77,7 +77,7 @@ const universalScraper = async (ctx: MovieScrapeContext | ShowScrapeContext) => 
 export const ridooMoviesScraper = makeSourcerer({
   id: 'ridomovies',
   name: 'RidoMovies',
-  rank: 200,
+  rank: 202,
   flags: [],
   scrapeMovie: universalScraper,
   // scrapeShow: universalScraper,
